b9a1b0c0ba520e17eaed54a480b2ec0a4f4fc63b
cbenedikter <84837890+cbenedikter@users.noreply.github.com>
2024-12-02 16:42:29 +0000

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Show the login pop-up
+function showPopup() {
+    document.getElementById('popup').style.display = 'block';
+}
+
+// Hide the login pop-up
+function hidePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
